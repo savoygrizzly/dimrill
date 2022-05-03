a05afe009f72c1ad2dcc091d754c3b94dc2980f5
sosickstudio <laporte.mateo@gmail.com>
2022-05-03 21:12:03 +0200

    Schemas show problems

---
+++ src/example.dmrl.js
@@ -0,0 +1,5 @@
+const Dimrill = require("./index.js");
+const list = {
+  createOrder: ["priceList", "organization"],
+  sub,
+};
--- src/index.js
+++ src/index.js
@@ -36,14 +36,40 @@ function validate(statement, req, user, context) {
     return condition;
   }
 }
-
+function synthetize(str, req, service, paths = undefined) {
+  /* 
+    This function synthetizes drna from base string by adding parameters from req or the passed object
+    possible paths are either defined by passing a paths object or using a config file
+  */
+}
+function Schema(args) {
+  if (typeof args !== "object") {
+    throw new Error("Dimrill Schema must contain an Object");
+  }
+  Object.keys(args).forEach((key) => {
+    /*
+      Check wether key contains spaces or special characters
+    */
+    if (key.test(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
+      throw new Error(
+        `For key: ${key}; Dimrill Schema properties must be guidelines compliant`
+      );
+    }
+    /*
+      Check wether property's value is an Object or an Array
+    */
+    if(args[key]) 
+  });
+}
 function initialize(...args) {
   this.adapter = args[0].options
     ? Adapters[args[0].options.adapter] ?? Adapters.mongo
     : Adapters.mongo;
+  this.synthetizer;
   return this;
 }
 module.exports = {
   validate: validate,
   initialize: initialize,
+  synthetize: synthetize,
 };
