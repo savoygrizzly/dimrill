80ce12a44cf6d753c317b72789f63c0e43a08ec3
sosickstudio <laporte.mateo@gmail.com>
2022-05-11 23:36:49 +0200

    Schemas explaination

--- src/example.dmrl.js
+++ src/example.dmrl.js
@@ -1,15 +1,32 @@
 const Dimrill = require("./index.js");
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *              SCHEMAS IMPLEMETATION EXAMPLE
+ *
+ *
+ *
+ *    Schemas have to be generated using the Dimrill.Schema class
+ *    It would be best practice to separate each of them in  DIFFERENT file
+ *    Then to assign them all to a single object befor passing it as constructor
+ *
+ *
+ *
+ *
+ */
 const orders = new Dimrill.Schema(
   {
     createOrder: ["priceList", "organization"],
     category: {
       subCategory: {
         subSubCategory: ["value One", "valueTwo"],
-        subSubCategory1: {
-          shit: "key",
-        },
+        subSubCategory1: true,
       },
     },
   },
-  "DEBUG"
+  { debug: true, strict: true }
 );
