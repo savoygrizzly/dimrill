9722959754ed55f9eedd06712382cda6ff4e6ad4
sosickstudio <laporte.mateo@gmail.com>
2022-05-25 02:44:01 +0200

    license added

--- examples/express/middlewares/auth.js
+++ examples/express/middlewares/auth.js
@@ -14,31 +14,32 @@ const authorizeToken = (drna_array) => {
       }
       const policies = jwt_payload.policies,
         user = jwt_payload.user;
-        /*
+      /*
         We might not need to pass the full req object to Dimrill, we will only pass the body if it's a post request,
         otherwise we'll pass the query params.
-        */      
+        */
       const formattedReq = req.method == "POST" ? req.body : req.query;
       const authorizer = Dimrill.authorize(
         drna_array,
         policies,
-        re
+        formattedReq,
         user,
         {}
       );
-      console.log(authorizer);
-
       if (authorizer.valid !== true) {
         return res.sendStatus(403);
       }
       /*
-        If authorizer is valid check if it has a query and attach it to res.locals to be used subsequently
+        If authorizer returns valid check if it has a query and attach it to res.locals to be used subsequently
       */
       if (
         typeof authorizer.query === "string" ||
         (typeof authorizer.query === "object" &&
           Object.keys(authorizer.query).length >= 1)
       ) {
+        /*
+          Let's create a property "dimrillQuery" so we can access it later
+        */
         res.locals.dimrill_query = authorizer.query;
       }
       next();
